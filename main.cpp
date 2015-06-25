b048ab1357f0dab3a125c688c7b6aaa0565b1e08
NPWR <npwrmusic@gmail.com>
2015-06-25 17:38:46 +0200

    Start

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+int main()
+{
+    RenderWindow app(VideoMode(1024,500,32),"Hello !");
+    app.setFramerateLimit(60);
+    app.setVerticalSyncEnabled(true);
+
+    while (app.isOpen())
+    {
+        Event event;
+        while (app.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+            {
+                app.close();
+            }
+        }
+    app.clear();
+
+    app.display();
+    }
+}
